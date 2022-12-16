b2cf73ec3aee4df1296d1a9807963c674f709554
Eksiart <eksiart-2@mail.ru>
2022-12-17 04:46:10 +0800

    hide files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/style.scss';
 import App from './components/app/App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM
   .createRoot(document.getElementById('root'))
